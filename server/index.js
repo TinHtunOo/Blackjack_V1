8ca560e1d0321fbfabfd8186192dcd6a6dea4557
TinHtunOo <tinhtunhe@gmail.com>
2026-09-14 20:50:26 +0700

    deployment

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
 import gameRouter from "../routes/gameRoutes.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+console.log(PORT);
 app.use(cors());
 app.use(express.json());
 
